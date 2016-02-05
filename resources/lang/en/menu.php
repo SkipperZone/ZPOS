57794493c16150dfc61ab12eaf2bef83de15fa32
Wahyu Trianna <whendro1@gmail.com>
2016-02-05 21:44:52 +0700

    update language menu

--- resources/lang/en/menu.php
+++ resources/lang/en/menu.php
@@ -1,19 +1,43 @@
 <?php
 
-return [
-
-	'dashboard' 			=> 'Dashboard',
+'dashboard' 			=> 'Dashboard',
 	'customers' 			=> 'Customers',
 	'employees'				=> 'Employees',
 	'items' 				=> 'Items',
-	'item_kits' 			=> 'Item Kits',
+	'item_kits' 			=> 'Items Kits',
 	'suppliers' 			=> 'Suppliers',
 	'receivings' 			=> 'Receivings',
 	'sales' 				=> 'Sales',
 	'reports' 				=> 'Reports',
-	'receivings_report' 	=> 'Receivings Report',
-	'sales_report' 			=> 'Sales Report',
+	'receivings_report' 	=> 'Receivings Reports',
+	'sales_report' 			=> 'Sales Reports',
 	'logout'				=> 'Logout',
-	'application_settings' 	=> 'Application Settings'
+	'application_settings' 	=> 'Application Settings',
+	//bypassing menu
+	'root'					=> 'Master',
+	'rep'					=> 'Reports',
+	'trans'					=> 'Transactions',
+	//'rep'					=> 'Laporan',
+	'return'				=> 'Return',
+
+	//persediaan
+	'stock_menu' 			=> 'Stock',
+	'warehouse' 			=> 'Warehouse',
+	'item_groups' 			=> 'Item Group',
+	'item_pricelist'		=> 'Item Pricelist',
+	'move_warehouse'		=> 'Move Warehouse',
+	'stock_corrections'		=> 'Stock Corrections',
+	'stock_cards' 			=> 'Stock Cards',
+	'stock_items' 			=> 'Stock Items',
+	//pembelian
+	'purchase'				=> 'Purchase',
+	'payment_hutang'		=> 'Debt Payment',
+	'hutang'				=> 'Debt',
+	//Penjualan
+	'sale_person'			=> 'Sales',
+	'retail'				=> 'Retail',
+	'grocery'				=> 'Grocery',
+	'piutang'				=> 'Credit',
+	'receivings_piutang'	=> 'Receivings Credit',
 
 ];
